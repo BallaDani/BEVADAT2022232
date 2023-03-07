a4cac891d2baafb3e52f60bb3808f9945c3d81f9
BallaDani <balladanivadonjaro@gmail.com>
2023-03-07 21:16:32 +0100

    Removez "%%"

--- GYAK/GYAK02/GYAK02.py
+++ GYAK/GYAK02/GYAK02.py
@@ -1,7 +1,7 @@
-# %%
+
 import numpy as np
 
-# %%
+
 #FONTOS!!!
 
 # CSAK OTT LEHET HASZNÁLNI FOR LOOP-OT AHOL A FELADAT KÜLÖN KÉRI!
@@ -9,61 +9,59 @@ import numpy as np
 # Ha végeztél a feladatokkal, akkor notebook-ot alakítsd át .py.
 # A FÁJLBAN CSAK A FÜGGVÉNYEK LEGYENEK! (KOMMENTEK MARADHATNAK)
 
-# %%
+
 #Készíts egy függvényt ami létre hoz egy nullákkal teli numpy array-t.
 #Paraméterei: mérete (tuple-ként), default mérete pedig legyen egy (2,2)
 #Be: (2,2)
 #Ki: [[0,0],[0,0]]
 #create_array()
 
-# %%
+
 def create_array(size=(2,2))->np.array:
     array=np.array(size)
     return array
 
-# %%
+
 #Készíts egy függvényt ami a paraméterként kapott array-t főátlóját feltölti egyesekkel
 #Be: [[1,2],[3,4]]
 #Ki: [[1,2],[3,1]]
 #set_one()
 
-# %%
 def set_one(input_array:np.array)->np.array:
     np.fill_diagonal(input_array,1)
     return input_array    
         
         
 
-# %%
+
 # Készíts egy függvényt ami transzponálja a paraméterül kapott mártix-ot:
 # Be: [[1, 2], [3, 4]]
 # Ki: [[1, 2], [3, 4]]
 # do_transpose()
 
-# %%
+
 def do_transpose(input_matrix:np.array)->np.array:
     input_matrix=np.transpose(input_matrix)
     return input_matrix
     
 
-# %%
+
 # Készíts egy olyan függvényt ami az array-ben lévő értékeket N tizenedjegyik kerekíti, ha nincs megadva ez a paraméter, akkor legyen az alapértelmezett a kettő 
 # Be: [0.1223, 0.1675], 2
 # Ki: [0.12, 0.17]
 # round_array()
 
-# %%
+
 def round_array(array, n:int=2)->np.array:
     
     return np.round(array,n)
 
-# %%
 # Készíts egy olyan függvényt, ami a bementként kapott 0 és 1 ből álló tömben a 0 - False-ra, az 1 True-ra cserélni
 # Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
 # Ki: [[ True False False], [ True  True  True], [False False False]]
 # bool_array()
 
-# %%
+
 
 def bool_array(array:np.array)->np.array:
     array=np.array(array,dtype=bool)
@@ -71,26 +69,25 @@ def bool_array(array:np.array)->np.array:
     
 
 
-# %%
 # Készíts egy olyan függvényt, ami a bementként kapott 0 és 1 ből álló tömben a 1 - False-ra az 0 True-ra cserélni
 # Be: [[1, 0, 0], [1, 1, 1],[0, 0, 0]]
 # Ki: [[ True False False], [ True  True  True], [False False False]]
 # invert_bool_array()
 
-# %%
+
 def invert_bool_array(array:np.array)->np.array:
     array=np.invert(np.array(array,dtype=bool))
     
     return array
 
-# %%
+
 # Készíts egy olyan függvényt ami a paraméterként kapott array-t kilapítja
 # Be: [[1,2], [3,4]]
 # Ki: [1,2,3,4]
 # flatten()
 
 
-# %%
+
 def flatten(input:np.array):
     return input.flatten()
 
