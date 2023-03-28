1224b1a8b1e9f1da3b2b84503ab7cc043206f642
BallaDani <balladanivadonjaro@gmail.com>
2023-03-28 11:22:41 +0200

    gyak05

---
+++ GYAK/GYAK05/GYAK05.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from typing import Tuple
+from scipy.stats import mode
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+class KNNClassifier:
+    
